632f5990e71d52e06cce2c576e1e87c3371295a6
Wasiu-hub <contactbow@yahoo.com>
2023-12-16 21:29:05 +0000

    open browser with custom locator

---
+++ readme.md
@@ -0,0 +1,3 @@
+// tagName[@att= 'attVal']
+
+//button[@id='onetrust-accept-btn-handler']-- custom locator for accept cookies button
--- src/test/java/com/next/steps/Search.java
+++ src/test/java/com/next/steps/Search.java
@@ -3,17 +3,23 @@ package com.next.steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.messages.types.Duration;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static io.cucumber.messages.types.Duration.*;
 
 public class Search {
     @Given("I am on the next home page")
     public void i_am_on_the_next_home_page() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://www.next.co.uk");
+        driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
     }
 
     @When("I enter {string} as a product")
