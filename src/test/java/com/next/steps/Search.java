75f837088dff0e04284b6dac121ef4813553dba2
Wasiu-hub <contactbow@yahoo.com>
2023-12-16 21:03:45 +0000

    runner, feature and step definition files created with open browser

---
+++ src/test/java/com/next/steps/Search.java
@@ -0,0 +1,34 @@
+package com.next.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Search {
+    @Given("I am on the next home page")
+    public void i_am_on_the_next_home_page() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.next.co.uk");
+    }
+
+    @When("I enter {string} as a product")
+    public void i_enter_as_a_product(String string) {
+
+    }
+
+    @When("I click on search button")
+    public void i_click_on_search_button() {
+
+    }
+
+    @Then("I should be able to see {string} as a result tiltle")
+    public void i_should_be_able_to_see_as_a_result_tiltle(String string) {
+
+    }
+
+}
