d8fbf7648908d0d05ae11389b784b19753b98dad
Wasiu-hub <contactbow@yahoo.com>
2023-12-21 20:16:30 +0000

    more refactoring to make code reusable

--- src/test/java/com/next/page/SearchPage.java
+++ src/test/java/com/next/page/SearchPage.java
@@ -7,4 +7,18 @@ public class SearchPage {
     public void acceptCookies(WebDriver driver){
         driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
     }
+
+    public void sendProductTypeInSearchBox(WebDriver driver, String productType){
+        driver.findElement(By.xpath("//input[@id='header-big-screen-search-box']")).sendKeys(productType);
+
+    }
+
+    public void clickOnSearch(WebDriver driver) {
+        driver.findElement(By.xpath("//button[@data-testid='header-search-bar-button']/img")).click();
+
+    }
+
+    public String getResultTitle(WebDriver driver) {
+       return driver.findElement(By.xpath("//span[@data-testid= 'plp-product-title-text']")).getText();
+    }
 }
--- src/test/java/com/next/steps/Search.java
+++ src/test/java/com/next/steps/Search.java
@@ -13,37 +13,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import static io.cucumber.messages.types.Duration.*;
 
-public class Search extends CommonFunctions{
+public class Search extends CommonFunctions {
+
+    SearchPage searchPage = new SearchPage();
+
     @Given("I am on the next home page")
     public void i_am_on_the_next_home_page() {
-        SearchPage searchPage = new SearchPage();
         searchPage.acceptCookies(driver);
     }
 
     @When("I enter {string} as a product")
     public void i_enter_as_a_product(String productType) {
-        driver.findElement(By.xpath("//input[@id='header-big-screen-search-box']")).sendKeys(productType);
+        searchPage.sendProductTypeInSearchBox(driver, productType);
     }
 
     @When("I click on search button")
     public void i_click_on_search_button() {
-//        driver.findElement(By.xpath("//img[@alt='Search Icon']")).click();
-//        driver.findElement(By.xpath("//img[@src='/static-content/icons/header/next/v1/vision/search-input-button.svg']")).click();
-
-        driver.findElement(By.xpath("//button[@data-testid='header-search-bar-button']/img")).click();
+        searchPage.clickOnSearch(driver);
     }
 
-    @Then("I should be able to see {string} as a result tiltle")
+    @Then("I should be able to see {string} as a result title")
     public void i_should_be_able_to_see_as_a_result_title(String resultTitle) {
 
         String expectedValue = "\"" + resultTitle + "\"";
-//        Thread.sleep(2000);
-        String actualValue = driver.findElement(By.xpath("//span[@data-testid= 'plp-product-title-text']")).getText();
-
+        String actualValue = searchPage.getResultTitle(driver);
         Assert.assertEquals(expectedValue, actualValue);
 
-    }
-    @Then("I should be able to see {string} as a result title")
-    public void iShouldBeAbleToSeeAsAResultTitle(String string) {
     }
 }
