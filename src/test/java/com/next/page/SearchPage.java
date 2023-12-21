7ae865fe616eb406b6136db0f31e03774688bffe
Wasiu-hub <contactbow@yahoo.com>
2023-12-21 21:10:28 +0000

    using 'this' keyword for easy identification

--- src/test/java/com/next/page/SearchPage.java
+++ src/test/java/com/next/page/SearchPage.java
@@ -1,24 +1,31 @@
 package com.next.page;
 
+import com.sun.jna.WString;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SearchPage {
-    public void acceptCookies(WebDriver driver){
+
+    public WebDriver driver;
+
+    public SearchPage(WebDriver driver){
+        this.driver = driver;
+
+    }
+    public void acceptCookies(){
         driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
     }
 
-    public void sendProductTypeInSearchBox(WebDriver driver, String productType){
+    public void sendProductTypeInSearchBox(String productType){
         driver.findElement(By.xpath("//input[@id='header-big-screen-search-box']")).sendKeys(productType);
 
     }
 
-    public void clickOnSearch(WebDriver driver) {
+    public void clickOnSearch() {
         driver.findElement(By.xpath("//button[@data-testid='header-search-bar-button']/img")).click();
 
     }
-
-    public String getResultTitle(WebDriver driver) {
+    public String getResultTitle() {
        return driver.findElement(By.xpath("//span[@data-testid= 'plp-product-title-text']")).getText();
     }
 }
--- src/test/java/com/next/steps/Search.java
+++ src/test/java/com/next/steps/Search.java
@@ -15,28 +15,28 @@ import static io.cucumber.messages.types.Duration.*;
 
 public class Search extends CommonFunctions {
 
-    SearchPage searchPage = new SearchPage();
+    SearchPage searchPage = new SearchPage(driver);
 
     @Given("I am on the next home page")
     public void i_am_on_the_next_home_page() {
-        searchPage.acceptCookies(driver);
+        searchPage.acceptCookies();
     }
 
     @When("I enter {string} as a product")
     public void i_enter_as_a_product(String productType) {
-        searchPage.sendProductTypeInSearchBox(driver, productType);
+        searchPage.sendProductTypeInSearchBox(productType);
     }
 
     @When("I click on search button")
     public void i_click_on_search_button() {
-        searchPage.clickOnSearch(driver);
+        searchPage.clickOnSearch();
     }
 
     @Then("I should be able to see {string} as a result title")
     public void i_should_be_able_to_see_as_a_result_title(String resultTitle) {
 
         String expectedValue = "\"" + resultTitle + "\"";
-        String actualValue = searchPage.getResultTitle(driver);
+        String actualValue = searchPage.getResultTitle();
         Assert.assertEquals(expectedValue, actualValue);
 
     }
