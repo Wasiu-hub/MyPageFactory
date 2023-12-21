dc931ca88a5b52c56d1244ea1b542eb6ef6c0d63
Wasiu-hub <contactbow@yahoo.com>
2023-12-21 21:43:07 +0000

    assigning values using Pagefactory, test passes

--- src/test/java/com/next/page/SearchPage.java
+++ src/test/java/com/next/page/SearchPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class SearchPage {
 
@@ -12,7 +13,7 @@ public class SearchPage {
 
     public SearchPage(WebDriver driver) {
         this.driver = driver;
-
+        PageFactory.initElements(driver, this);
     }
 
     @FindBy(xpath = "//button[@id='onetrust-accept-btn-handler']")
