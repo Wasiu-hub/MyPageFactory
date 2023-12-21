94f178179c818e09f79486196f031b1ae3872050
Wasiu-hub <contactbow@yahoo.com>
2023-12-21 21:31:28 +0000

    simplifying code using annotation, but test fails

--- src/test/java/com/next/page/SearchPage.java
+++ src/test/java/com/next/page/SearchPage.java
@@ -3,29 +3,45 @@ package com.next.page;
 import com.sun.jna.WString;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class SearchPage {
 
     public WebDriver driver;
 
-    public SearchPage(WebDriver driver){
+    public SearchPage(WebDriver driver) {
         this.driver = driver;
 
     }
-    public void acceptCookies(){
-        driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
+
+    @FindBy(xpath = "//button[@id='onetrust-accept-btn-handler']")
+    private WebElement acceptBtn;
+
+    public void acceptCookies() {
+        acceptBtn.click();
     }
 
-    public void sendProductTypeInSearchBox(String productType){
-        driver.findElement(By.xpath("//input[@id='header-big-screen-search-box']")).sendKeys(productType);
+    @FindBy(xpath = "//input[@id='header-big-screen-search-box']")
+    private WebElement searchInp;
+
+    public void sendProductTypeInSearchBox(String productType) {
+        searchInp.sendKeys(productType);
 
     }
+
+    @FindBy(xpath = "//button[@data-testid='header-search-bar-button']/img")
+    private WebElement searchBtn;
 
     public void clickOnSearch() {
-        driver.findElement(By.xpath("//button[@data-testid='header-search-bar-button']/img")).click();
+        searchBtn.click();
 
     }
+
+    @FindBy(xpath = "//span[@data-testid= 'plp-product-title-text']")
+    private WebElement resultTitle;
+
     public String getResultTitle() {
-       return driver.findElement(By.xpath("//span[@data-testid= 'plp-product-title-text']")).getText();
+        return resultTitle.getText();
     }
 }
